b402f52b366ea3a5389eea7155aa62a8ca0efc47
MayHyeyeonKim <hkim7963@gmail.com>
2023-12-25 23:32:56 -0800

    JDK17 | Gradle JDK17 | Build and run using & Run tests using IntelliJ

---
+++ src/main/java/May/myFirstSpring/MyFirstSpringApplication.java
@@ -0,0 +1,14 @@
+package May.myFirstSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstSpringApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MyFirstSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/May/myFirstSpring/MyFirstSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package May.myFirstSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
