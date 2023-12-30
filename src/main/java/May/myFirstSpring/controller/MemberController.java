bbacd2048a8bce45f846758019f82dc3385c9a87
MayHyeyeonKim <hkim7963@gmail.com>
2023-12-29 23:35:52 -0800

    feat: create MemberRegistration with MemberForm

--- src/main/java/May/myFirstSpring/controller/MemberController.java
+++ src/main/java/May/myFirstSpring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package May.myFirstSpring.controller;
 
+import May.myFirstSpring.domain.Member;
 import May.myFirstSpring.servics.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -11,5 +14,20 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+        System.out.println("member = " + member.getName());
+        return "redirect:/";
     }
 }
---
+++ src/main/java/May/myFirstSpring/controller/MemberForm.java
@@ -0,0 +1,11 @@
+package May.myFirstSpring.controller;
+
+public class MemberForm {
+    private String name;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
