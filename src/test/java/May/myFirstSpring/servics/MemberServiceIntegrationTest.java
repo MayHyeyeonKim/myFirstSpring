ff55b2d32b9e7dfbcfa070b158f3bd2bd20bbfb5
MayHyeyeonKim <hkim7963@gmail.com>
2024-01-05 01:22:42 -0800

    MemberServiceIntegrationTest

---
+++ src/test/java/May/myFirstSpring/servics/MemberServiceIntegrationTest.java
@@ -0,0 +1,63 @@
+package May.myFirstSpring.servics;
+
+import May.myFirstSpring.domain.Member;
+import May.myFirstSpring.repository.MemberRepository;
+import May.myFirstSpring.repository.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    void join() {
+
+        //given
+        Member member = new Member();
+        member.setName("안녕쓰");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        Assertions.assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void DuplicateMemberException(){
+
+        //given
+        Member member1 = new Member();
+        member1.setName("안뇽쓰1");
+
+        Member member2 = new Member();
+        member2.setName("안뇽쓰1");
+
+        //when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
+    }
+
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
